c4495fa1583da0ea1870610d19e024a462544107
heliogabalo <jonitjuego@gmail.com>
2016-03-03 18:42:56 +0100

    Un iterador en ruby

---
+++ hash.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+puts "dime argo pallo"
+text = gets.chomp
+
+words = text.split(" ")
+frequencies = Hash.new(0)
+words.each do |x|
+   frequencies[x] += 1
+end
+frequencies = frequencies.sort_by {|a, b| b}
+frequencies.reverse!
+frequencies.each do |x, a|
+   puts x + " " + a.to_s
+end
